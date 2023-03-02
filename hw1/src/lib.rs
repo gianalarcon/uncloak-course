272cdcf21ac5dc2cbd00a44c7b7fa53e0ad83823
gianmarcoalarcon <gian.alarcon@unmsm.edu.pe>
2023-03-03 04:06:02 +0800

    Feat: Add decrypt(), test_vig()

--- hw1/src/lib.rs
+++ hw1/src/lib.rs
@@ -2,7 +2,9 @@ use rand::{rngs::ThreadRng, CryptoRng, Rng, RngCore};
 use rsa::{PaddingScheme, PublicKey, RsaPrivateKey, RsaPublicKey};
 
 pub mod vignere {
-  use std::iter::zip;
+  use std::{iter::zip, str::from_utf8};
+
+  use anyhow::Ok;
 
   #[derive(Clone, Debug)]
   pub struct Vignere<'a> {
@@ -30,15 +32,30 @@ pub mod vignere {
         key: key.as_bytes(),
       })
     }
-    pub fn encrypt(&self, plaintext: &str) -> anyhow::Result<Vec<u8>> {
+    pub fn encrypt(&self, plaintext: &str) -> anyhow::Result<String> {
       let key_it = self.key.iter().cycle();
       check_alphabetic(plaintext)?;
-      let output: Vec<_> = zip(key_it, plaintext.as_bytes())
+      let output = zip(key_it, plaintext.as_bytes())
         .map(|(k, p)| {
           let k = k - b'a';
           let p = p - b'a';
-          let c = (k + p) % 26;
-          c + b'a'
+          let mut c = (k + p) % 26;
+          c = c + b'a';
+          c as char
+        })
+        .collect();
+      Ok(output)
+    }
+    pub fn decrypt(&self, cyphertext: &str) -> anyhow::Result<String> {
+      let key_it = self.key.iter().cycle();
+      check_alphabetic(cyphertext)?;
+      let output = zip(key_it, cyphertext.as_bytes())
+        .map(|(k, c)| {
+          let k = k - b'a';
+          let c = c - b'a';
+          let mut p = (c + 26 - k) % 26;
+          p = p + b'a';
+          p as char
         })
         .collect();
       Ok(output)
@@ -54,8 +71,9 @@ pub mod vignere {
       let key = "lemon";
       let v = Vignere::new(key).unwrap();
       let ciphertext = v.encrypt(msg).unwrap();
-      let test_ciphertext = "lxfopvefrnhr".as_bytes().to_vec();
-      assert_eq!(ciphertext, test_ciphertext);
+      let ciphertext = ciphertext.as_str();
+      let plaintext = v.decrypt(ciphertext).unwrap();
+      assert_eq!(msg, plaintext);
     }
   }
 }
